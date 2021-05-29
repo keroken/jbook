f88cab04239acd1458a1951807813693801fe2e7
keroken <kenichi.shinagawa@gmail.com>
2021-05-30 00:43:28 +0900

    umake esbuild usable from components

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,30 @@
-import { useState } from 'react';
+import * as esbuild from 'esbuild-wasm';
+import { useEffect, useRef, useState } from 'react';
 import ReactDOM from 'react-dom';
 
 const App = () => {
+  const ref = useRef<any>();
   const [input, setInput] = useState('');
   const [code, setCode] = useState('');
 
+  const startService = async () => {
+    ref.current = await esbuild.startService({
+      worker: true,
+      wasmURL: '/esbuild.wasm'
+    });
+  };
+
+  useEffect(() => {
+    startService();
+  }, []);
+
   const onClick = () => {
-    console.log(input);
-  }
+    if (!ref.current) {
+      return;
+    }
+
+    console.log(ref.current);
+  };
 
   return <div>
     <textarea value={input} onChange={e => setInput(e.target.value)}></textarea>
