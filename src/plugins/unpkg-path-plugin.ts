39bd73e4cb75753ac5844560e6e1be302df73f2a
keroken <kenichi.shinagawa@gmail.com>
2021-06-05 23:13:35 +0900

    use input as argument for the plugin

--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -6,7 +6,7 @@ const fileCache = localforage.createInstance({
   name: 'filecache'
 });
 
-export const unpkgPathPlugin = () => {
+export const unpkgPathPlugin = (inputCode: string) => {
   return {
     name: 'unpkg-path-plugin',
     setup(build: esbuild.PluginBuild) {
@@ -35,10 +35,7 @@ export const unpkgPathPlugin = () => {
         if (args.path === 'index.js') {
           return {
             loader: 'jsx',
-            contents: `
-              import message from 'nested-test-pkg';
-              console.log(message);
-            `,
+            contents: inputCode,
           };
         }
 
