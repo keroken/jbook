3d3c4fda8f02d5b2962c3f0333443bb9826a45dd
keroken <kenichi.shinagawa@gmail.com>
2021-07-10 17:20:56 +0900

    add public/text.html and load iframe from local data

--- src/index.tsx
+++ src/index.tsx
@@ -48,8 +48,13 @@ const App = () => {
       <button onClick={onClick}>Submit</button>
     </div>
     <pre>{code}</pre>
+    <iframe sandbox="" title="iframe" srcDoc={html} />
   </div>;
 };
+
+const html = `
+  <h1>Local HTML</h1>
+`;
 
 ReactDOM.render(
   <App />,
